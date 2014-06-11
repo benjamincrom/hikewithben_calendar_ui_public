5aa76cce852cdab3ecfe3cfbbba5fb4793aca981
Benjamin Crom <bcrom@syapse.com>
2014-06-11 14:30:24 -0700

    Handlebars switch

--- app/js/app.js
+++ app/js/app.js
@@ -1,52 +1,21 @@
-'use strict';
+var source = $("#hike-list-template").html(); 
+var template = Handlebars.compile(source); 
 
-/* App */
-var hikeWithBenApp = angular.module('hikeWithBenApp', [
-    'ui.bootstrap',
-    'ngSanitize',
-    'ngResource',
-]);
+function getHikeList() {
+    var response = '';
+    $.ajax({ type: "GET",   
+             url: "https://hikewithben.appspot.com/_ah/api/hikewithben/v1/hike_list",
+             async: false,
+             success : function(text)
+             {
+                 response = text;
+             }
+    });
 
-/* Config */
-hikeWithBenApp.config(function($locationProvider){
-    $locationProvider.html5Mode(true).hashPrefix('!');
-});
+    return response;
+}
 
-/* Controllers */
-hikeWithBenApp.controller('HikeListCtrl',
-    function getHikeList($scope, $http, $sce) {
-        $scope.trustSrc = function(src) {
-          return $sce.trustAsResourceUrl(src);
-        }
-        $http.get('https://hikewithben.appspot.com/_ah/api/hikewithben/v1/hike_list').
-            success(function(data) {
-                $scope.hike_list = data.hike_list;
-            });
-    }
-);
+var data = getHikeList(); 
+var html = template(data)
+$("#content-placeholder").html(html);
 
-hikeWithBenApp.controller('DisplayHikeCtrl',
-    function DisplayHikeCtrl($scope, $http, $location) {
-        var link_prefix = "https://hikewithben.appspot.com/_ah/api/hikewithben/v1/hike_display/";
-        var link = link_prefix.concat($location.search().hike_id)
-        $http.get(link).success(function(data) {
-            $scope.hike = data;
-        });
-    }
-);
-
-/* Directives */
-hikeWithBenApp.directive('loadingContainer', function () {
-    return {
-        restrict: 'A',
-        scope: false,
-        link: function(scope, element, attrs) {
-            var loadingLayer = angular.element('<div class="loading"></div>');
-            element.append(loadingLayer);
-            element.addClass('loading-container');
-            scope.$watch(attrs.loadingContainer, function(value) {
-                loadingLayer.toggleClass('ng-hide', !value);
-            });
-        }
-    };
-});
